35bb4f99463a58ce932cf0b310fd605436054836
TakahiroKida <48724942+TakahiroKida@users.noreply.github.com>
2019-03-27 21:33:01 +0900

    FirstCommit

---
+++ HttpReqestsBot/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace HttpReqestsBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1, 253);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(383, 307);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(360, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "一周のみリクエスト送信";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "秒おきにリクエスト送信";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(12, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(360, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(12, 224);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "URLリスト読み込み";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Meiryo UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(12, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 48);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "60";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(270, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "クリア";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "うどん用糞便利ツール";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HttpReqestsBot/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using SHDocVw;
+using System.Runtime.InteropServices;
+
+
+namespace HttpReqestsBot
+{
+    public partial class Form1 : Form
+    {
+        //IEの入れ物
+        SHDocVw.InternetExplorer ie = null;
+        //テキストボックス
+        StringReader strReader = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //フォームサイズ固定
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+            //ファイル読み込みダイアログの設定
+            openFileDialog1.Title = "URLリストを選択してください。";
+            openFileDialog1.FileName = "URL_List.txt";
+            openFileDialog1.Filter = "てきすとファイル(*.txt)|*.txt;|すべてのファイル(*.*)|*.*";
+            //ボタン関連
+            button3.Enabled = false;
+        }
+
+        //ファイル読み込み
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //ファイルダイアログが正常に閉じた場合
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //テキストボックスにURL一覧表示
+                StreamReader sr = new StreamReader(openFileDialog1.FileName, Encoding.GetEncoding("Shift_JIS"));
+
+                richTextBox1.Text = sr.ReadToEnd();
+
+                sr.Close();
+            }
+        }
+        //テキストボックス全消し
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+        //一回のみ起動
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            ie = new SHDocVw.InternetExplorer();
+            ie.Visible = false;
+            read_req_file();
+            ie.Quit();
+            button1.Enabled = true;
+        }
+        //ファイル読み込みからのリクエスト
+        private void read_req_file()
+        {
+            strReader = new StringReader(richTextBox1.Text);
+            //末端までr－プ
+            while (strReader.Peek() > -1)
+            {
+                http_Req(strReader.ReadLine());
+            }
+            strReader.Close();
+        }
+        //HTTPリクエスト
+        private void http_Req(String URLStr)
+        {
+            while(ie.Busy)
+            {
+            }
+            //リクエスト
+            ie.Navigate(URLStr);
+        }
+        //秒おきに実行
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button3.Enabled = true;
+            ie = new SHDocVw.InternetExplorer();
+            ie.Visible = false;
+            //タイマーの秒設定
+            timer1.Interval = 1000 * int.Parse(textBox1.Text);
+            timer1.Enabled = true;
+            read_req_file();
+        }
+        //タイマー
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            read_req_file();
+        }
+        //停止ボタン
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = false;
+            ie.Quit();
+        }
+    }
+}
